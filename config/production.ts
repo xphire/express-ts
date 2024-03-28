0d85c99ffee92d6e1e216224dd3cfffb8b686206
Abdulafeez Adetunmbi <xplicifire@gmail.com>
2024-03-28 07:01:36 +0100

    finally deployed

--- config/production.ts
+++ config/production.ts
@@ -2,3 +2,7 @@ export default{
     port : 9500,
     mongoUri : process.env.MONGO_URI
 }
+
+
+//mongodb://<user>:<pass>@cluster0.<server>.mongodb.net/<mydb>?retryWrites=true&w=majority sample connection string
+
